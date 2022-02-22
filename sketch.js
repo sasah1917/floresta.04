9124240853db5c8777a3fbca60569e6b1baf6975
sasah1917 <92746412+sasah1917@users.noreply.github.com>
2022-02-22 20:19:45 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var menino, meninoImg;
+var floresta;
+var borboleta,  borboletaImg;
+
+function preload() {
+    floresta = loadImage("floresta.jpg");
+}
+
+function setup() {
+    createCanvas(800, 400);
+
+
+}
+
